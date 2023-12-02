4a0061cdd962500302c62f731067a7a66994f543
Alissa <pattersonal10@gmail.com>
2023-12-02 12:00:33 -0600

    component files, pages and README

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# GoogleBookEngine
+# Google Book Search Engine
+
+![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)
+
+# Google Books Search and Save App
+
+An application that allows users to search for books using the Google Books API, save their favorite books to their profile, and view and manage their saved book list.
+
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Deployment](#deployment)
+- [License](#license)
+- [Contact](#contact)
+
+## Introduction
+
+This project is a Google book search engine built with GraphQL API and Apollo server. You can search for books and save them to your account. You can also remove the saved books from your list.
+
+
+## Features
+
+1. **User Authentication:**
+   - Allow users to sign up and log in.
+   - Secure user authentication using tokens.
+
+2. **Search Books:**
+   - Utilize the Google Books API to search for books.
+   - Display search results with book details.
+
+3. **Save Books:**
+   - Allow users to save books to their profile.
+   - Store saved books in the database.
+
+4. **View Saved Books:**
+   - Provide a page to view books saved by the user.
+   - Display detailed information about each saved book.
+
+5. **Delete Saved Books:**
+   - Enable users to remove books from their saved list.
+   - Update the database when a book is deleted.
+
+6.  **Technologies:**
+    - Utilize technologies such as React, Apollo Client, and GraphQL.
+    - Incorporate server-side logic with Node.js and MongoDB for data storage.
+
+
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/Apatterson32/GoogleBookEngine.git
+
+Install the dependencies using `npm i`
+
+To start the application, run the following command: `npm run develop`
+
+
+## Deployment
+
+The application is deployed using [Render](https://render.com). Access the live deployment at [Google Book Search Engine]().
+
+![Screenshot 2023-12-01 235056](https://github.com/Apatterson32/GoogleBookEngine/assets/135552416/cf0a5c4b-8e50-44ba-bf33-c23f1f805e9b)
+
+
+
+## License
+
+This project is licensed under the MIT License.
+
+## Contact
+
+[Email](pattersonal10@gmail.com)
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -7,27 +7,23 @@ import {
   Card,
   Row
 } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
-  // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
-  // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
-  // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
-  // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
-  // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+  // Initialize the useMutation hook for SAVE_BOOK mutation
+  const [saveBook] = useMutation(SAVE_BOOK);
+
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
 
-  // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
@@ -36,12 +32,14 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await searchGoogleBooks(searchInput);
+      const response = await fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+      );
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
-
+      // response for 'fetch' is here
       const { items } = await response.json();
 
       const bookData = items.map((book) => ({
@@ -51,20 +49,18 @@ const SearchBooks = () => {
         description: book.volumeInfo.description,
         image: book.volumeInfo.imageLinks?.thumbnail || '',
       }));
-
+      // save searched books to react State
       setSearchedBooks(bookData);
+      // clear the search input field
       setSearchInput('');
     } catch (err) {
       console.error(err);
     }
   };
 
-  // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
-    // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
-    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -72,13 +68,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { bookInput: { ...bookToSave } }
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
     } catch (err) {
       console.error(err);
