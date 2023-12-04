e96d58476cacc090af67946920f41d466dcba87b
Alissa <pattersonal10@gmail.com>
2023-12-03 22:48:18 -0600

    reverted

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,15 +1,7 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine');
-
-// module.exports = mongoose.connection;
-
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine');
 
 module.exports = mongoose.connection;
+
 
