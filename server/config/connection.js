39f52cd7a5ab361dd0e1c8458dae9a71c4273650
Alissa <pattersonal10@gmail.com>
2023-12-03 22:37:39 -0600

    make better

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine');
+
+// module.exports = mongoose.connection;
+
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/booksearchengine', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 module.exports = mongoose.connection;
+
